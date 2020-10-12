b594422521ccd8823fa62ca0124a7aad61aaac18
ilovemanches <ilovemanches@naver.com>
2020-10-12 22:33:00 +0900

    Write Test code for PostsRepository

---
+++ src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.contentOf;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+    @Test
+    public void 게시글저장_불러오기(){
+        String title = "테스트 게시길";
+        String Content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+        .title(title)
+        .content(Content)
+        .author("siryoung97@gmail.com")
+        .build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(Content);
+    }
+}
